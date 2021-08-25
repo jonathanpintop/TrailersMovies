820dc055c93ed5422fa167839d0880a46a1fa09f
Jonathan Pinto <jonathanpp@gmail.com>
2021-08-25 10:40:02 -0500

    refactor: index.html and app.js

--- js/app.js
+++ js/app.js
@@ -2,115 +2,67 @@
 
 export const apiKey = "07b870c21213848a4a5f99985d44b475";
 const mainUrl = "https://api.themoviedb.org/3";
-const apiUrl = mainUrl + "/discover/movie?sort_by=popularity.desc&api_key="+apiKey;
+const apiUrl =
+  mainUrl + "/discover/movie?sort_by=popularity.desc&api_key=" + apiKey;
 export const mainImg = "https://image.tmdb.org/t/p/w500";
-const mainContainer = document.getElementById('movies-container');
-
+const mainContainer = document.getElementById("movies-container");
 
 // Form variables
-export const form = document.getElementById('form__banner');
-export const  formInput = document.getElementById("form__banner-search")
-export const buttonBanner = document.getElementById("button_banner")
+export const form = document.getElementById("form__banner");
+export const formInput = document.getElementById("form__banner-search");
+export const buttonBanner = document.getElementById("button_banner");
 
 // Form EventListener
-export const formEventListener = form.addEventListener('submit', searchInfo);
+export const formEventListener = form.addEventListener("submit", searchInfo);
 
 //Functions
-export function searchInfo(e){
-  e.preventDefault()
+export function searchInfo(e) {
+  e.preventDefault();
   const userSearch = formInput.value;
 
-  const searchApiUrl = mainUrl +"/search/movie?api_key="+apiKey + "&query="+userSearch
-
-
-
-
-
+  const searchApiUrl =
+    mainUrl + "/search/movie?api_key=" + apiKey + "&query=" + userSearch;
 
   searchApi(searchApiUrl);
 
-
   function searchApi(url) {
-    fetch(url).then(res => res.json()).then(data => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        clearHtml();
 
-    
-      
-
-      clearHtml();
-
-      
-      showMovies(data.results);
-
-
-
-
-    })
-
+        showMovies(data.results);
+      });
+  }
 }
 
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- moviesApi(apiUrl);
+moviesApi(apiUrl);
 
 function moviesApi(url) {
-    fetch(url).then(res => res.json()).then(data => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
       showMovies(data.results);
-    })
-
+    });
 }
 
 function showMovies(data) {
+  data.forEach((movie) => {
+    const { title, poster_path, vote_average, overview, release_date, id } =
+      movie;
 
- 
-        data.forEach(movie => {
-          const {title,poster_path, vote_average,overview,release_date,id} = movie
-       
-        const movieId = id;
+    const movieId = id;
 
+    let date = new Date(release_date);
 
-
-
-
-
-
-
-          let date = new Date(release_date);
-          
-        
-
-
-
-
-
-
-          const movieCard = document.createElement('div');
-            movieCard.classList.add('movie');
-            movieCard.innerHTML = `
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie");
+    movieCard.innerHTML = `
 
             <a  title="${title}">
-              <img data-id= "${id}" class="movie__img"  src="${mainImg + poster_path}" alt="${title}">
+              <img data-id= "${id}" class="movie__img"  src="${
+      mainImg + poster_path
+    }" alt="${title}">
             </a>
 
             <div class="movie__info">
@@ -120,56 +72,32 @@ function showMovies(data) {
             <div class="movie__date">
              ${date}
             </div>
-            `
-            mainContainer.appendChild(movieCard)
-            const movieImg = document.querySelectorAll('.movie__img');
-            const movieTitle = document.querySelectorAll('.movie__info--title');
-           
-           
-          movieImg.forEach (item =>{
-              item.addEventListener('click', movieSelected)
-          })
-          
-    
-        });
+            `;
+    mainContainer.appendChild(movieCard);
+    const movieImg = document.querySelectorAll(".movie__img");
+    const movieTitle = document.querySelectorAll(".movie__info--title");
 
-          
-
-
-         
+    movieImg.forEach((item) => {
+      item.addEventListener("click", movieSelected);
+    });
+  });
 }
 
-
-
-
 export function getColor(vote) {
-  if(vote >= 8){
-    return "green"
-  }else if(vote >= 5){
-    return "orange"
-  }else{
-    return "red"
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
   }
 }
 
-
-
-
-
-
-
-export function movieSelected(e){
-  const movieId = e.target.dataset.id
-  window.location=`movie.html?id=${movieId}`
-
-
-
-
-
+export function movieSelected(e) {
+  const movieId = e.target.dataset.id;
+  window.location = `movie.html?id=${movieId}`;
 }
 
-
-function  clearHtml(){
-  mainContainer.innerHTML="";
+function clearHtml() {
+  mainContainer.innerHTML = "";
 }
-
